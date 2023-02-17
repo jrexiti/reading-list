425165c83bb34e25dff1adaebc70ae0dea6eb3c1
Jay Rexiti <jayrexiti@Jays-MBP.attlocal.net>
2023-02-17 17:28:19 -0500

    handleSubmit

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function BookEdit() {
-  return <div>Book Edit</div>;
+  const [title, setTitle] = useState("");
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+  return (
+      <form onSubmit={ handleSubmit} className="book-edit">
+      <label>Title</label>
+          <input className="input" value={ title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
